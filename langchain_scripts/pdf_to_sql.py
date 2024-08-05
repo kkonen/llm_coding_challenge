f2127a30cc0bc2c6c7daff9062179918e158a375
Kai Konen <kkonen@gmail.com>
2024-08-05 13:10:32 +0200

    Add dummy script for enhanced agent functionality

---
+++ langchain_scripts/pdf_to_sql.py
@@ -0,0 +1,17 @@
+# Extract Data from PDF to JSON via LLM
+
+# Create SQL Database from JSON
+
+# Translate plain text query to SQL query
+
+# Query SQL Database
+
+# Translate SQL result to plain text via LLM
+
+
+
+
+
+
+def create_sql_query(args):
+    return "XBO 2000 W/HTP XL OFR"
